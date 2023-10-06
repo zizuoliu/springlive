487f090893c0e2b2eb2c2fb2e7fffe2687e7a789
zizuo <liuzizuo@gmail.com>
2023-10-05 20:13:02 -0700

    use requestMapping with method

--- springmono/src/main/java/net/nvsoftware/springmono/controller/HomeController.java
+++ springmono/src/main/java/net/nvsoftware/springmono/controller/HomeController.java
@@ -3,6 +3,7 @@ package net.nvsoftware.springmono.controller;
 import net.nvsoftware.springmono.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ public class HomeController {
         return "NVSoftware Home";
     }
 
-    @RequestMapping("/user")
+    @RequestMapping(value="/user", method= RequestMethod.POST)
     public User user() {
         User user = new User();
         user.setId("3100");
