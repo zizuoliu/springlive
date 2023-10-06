2fa4d0416c5bccfe27762ce6e5c4f9b3045002e7
zizuo <liuzizuo@gmail.com>
2023-10-05 19:43:06 -0700

    springone setup

---
+++ springmono/src/main/java/net/nvsoftware/springmono/SpringmonoApplication.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.springmono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmonoApplication.class, args);
+	}
+
+}
---
+++ springmono/src/test/java/net/nvsoftware/springmono/SpringmonoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.springmono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
