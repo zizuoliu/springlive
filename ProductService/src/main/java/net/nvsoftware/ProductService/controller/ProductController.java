d7166a860625d5683f4aa754f0b7f517f45b74ce
zizuo <liuzizuo@gmail.com>
2023-10-06 20:12:14 -0700

    For ProductService CRUD, getById

--- ProductService/src/main/java/net/nvsoftware/ProductService/controller/ProductController.java
+++ ProductService/src/main/java/net/nvsoftware/ProductService/controller/ProductController.java
@@ -1,14 +1,12 @@
 package net.nvsoftware.ProductService.controller;
 
 import net.nvsoftware.ProductService.model.ProductRequest;
+import net.nvsoftware.ProductService.model.ProductResponse;
 import net.nvsoftware.ProductService.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
@@ -19,5 +17,11 @@ public class ProductController {
     public ResponseEntity<Long> addProduct(@RequestBody ProductRequest productRequest) {
         long id = productService.addProduct(productRequest);
         return new ResponseEntity<>(id, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProductResponseById(@PathVariable long id) {
+        ProductResponse productResponse = productService.getById(id);
+        return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
 }
