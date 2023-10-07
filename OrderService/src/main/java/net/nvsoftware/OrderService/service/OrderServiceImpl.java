1cb419c12822d029009181ece9c1756fae46b66f
zizuo <liuzizuo@gmail.com>
2023-10-06 22:03:24 -0700

    setup openFeigh and config to do inter microservice call by RPC not Rest ip+port, change ProductService to legal name by openFeigh no _ only -

--- OrderService/src/main/java/net/nvsoftware/OrderService/service/OrderServiceImpl.java
+++ OrderService/src/main/java/net/nvsoftware/OrderService/service/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package net.nvsoftware.OrderService.service;
 
 import lombok.extern.log4j.Log4j2;
+import net.nvsoftware.OrderService.client.ProductServiceFeignClient;
 import net.nvsoftware.OrderService.entity.OrderEntity;
 import net.nvsoftware.OrderService.model.OrderRequest;
 import net.nvsoftware.OrderService.repository.OrderRepository;
@@ -14,7 +15,8 @@ import java.time.Instant;
 public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderRepository orderRepository;
-
+    @Autowired
+    private ProductServiceFeignClient productServiceFeignClient;
     @Override
     public long placeOrder(OrderRequest orderRequest) {
         log.info("Start: OrderService placeOrder");
@@ -29,6 +31,9 @@ public class OrderServiceImpl implements OrderService {
         orderRepository.save(orderEntity);
         log.info("Process: OrderService placeOrder save orderEntity with orderId: " + orderEntity.getId());
         // call ProductService to check product quantity, if ok, reduce it, else throw not enough
+        productServiceFeignClient.reduceQuantity(orderEntity.getProductId(), orderEntity.getQuantity());
+        log.info("Process: OrderService placeOrder FeignCall ProductService reduceQuantity");
+
         // call PaymentService to charge, if Success, mark order PAID, else CANCELLED
         log.info("End: OrderService placeOrder");
         return orderEntity.getId();
