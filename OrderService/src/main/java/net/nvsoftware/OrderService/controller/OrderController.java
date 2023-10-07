6c7569b133d3e7e0edb93e07a1295700a94d0da6
zizuo <liuzizuo@gmail.com>
2023-10-06 21:11:09 -0700

    For OrderService CRUD, placeOrder with fake service layer behaviour

---
+++ OrderService/src/main/java/net/nvsoftware/OrderService/controller/OrderController.java
@@ -0,0 +1,24 @@
+package net.nvsoftware.OrderService.controller;
+
+import net.nvsoftware.OrderService.model.OrderRequest;
+import net.nvsoftware.OrderService.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping("/placeOrder")
+    public ResponseEntity<Long> placeOrder(@RequestBody OrderRequest orderRequest) {
+        long orderId = orderService.placeOrder(orderRequest);
+        return new ResponseEntity<>(orderId, HttpStatus.OK);
+    }
+}
---
+++ OrderService/src/main/java/net/nvsoftware/OrderService/entity/OrderEntity.java
@@ -0,0 +1,28 @@
+package net.nvsoftware.OrderService.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.Instant;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private long productId;
+    private long quantity;
+    private long totalAmount;
+    private Instant orderDate;
+    private String orderStatus;
+}
