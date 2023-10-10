ac9d5bd3da1b7cff81ac5eea3b02eeb93fd6d996
zizuo <liuzizuo@gmail.com>
2023-10-09 19:51:25 -0700

    For PaymentService, doPayment

--- PaymentService/src/main/java/net/nvsoftware/PaymentService/service/PaymentService.java
+++ PaymentService/src/main/java/net/nvsoftware/PaymentService/service/PaymentService.java
@@ -1,4 +1,7 @@
 package net.nvsoftware.PaymentService.service;
 
+import net.nvsoftware.PaymentService.model.PaymentRequest;
+
 public interface PaymentService {
+    long doPayment(PaymentRequest paymentRequest);
 }
