355a81d1889a16993ef8050e5035da0acc71e77f
zizuo <liuzizuo@gmail.com>
2023-10-09 20:34:18 -0700

    For PaymentService, add PaymentResponse and getPaymentDetailByOrderId

--- PaymentService/src/main/java/net/nvsoftware/PaymentService/service/PaymentService.java
+++ PaymentService/src/main/java/net/nvsoftware/PaymentService/service/PaymentService.java
@@ -1,7 +1,10 @@
 package net.nvsoftware.PaymentService.service;
 
 import net.nvsoftware.PaymentService.model.PaymentRequest;
+import net.nvsoftware.PaymentService.model.PaymentResponse;
 
 public interface PaymentService {
     long doPayment(PaymentRequest paymentRequest);
+
+    PaymentResponse getPaymentDetailByOrderId(long orderId);
 }
--- PaymentService/src/main/java/net/nvsoftware/PaymentService/service/PaymentServiceImpl.java
+++ PaymentService/src/main/java/net/nvsoftware/PaymentService/service/PaymentServiceImpl.java
@@ -3,6 +3,7 @@ package net.nvsoftware.PaymentService.service;
 import lombok.extern.log4j.Log4j2;
 import net.nvsoftware.PaymentService.entity.PaymentEntity;
 import net.nvsoftware.PaymentService.model.PaymentRequest;
+import net.nvsoftware.PaymentService.model.PaymentResponse;
 import net.nvsoftware.PaymentService.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,5 +30,21 @@ public class PaymentServiceImpl implements PaymentService {
 
         log.info("End: PaymentService doPayment with id: " + paymentEntity.getId());
         return paymentEntity.getId();
+    }
+
+    @Override
+    public PaymentResponse getPaymentDetailByOrderId(long orderId) {
+        log.info("Start: PaymentService getPaymentDetailByOrderId");
+        PaymentEntity paymentEntity = paymentRepository.findByOrderId(orderId);
+        PaymentResponse paymentResponse = PaymentResponse.builder()
+                .id(paymentEntity.getId())
+                .orderId(paymentEntity.getOrderId())
+                .paymentStatus(paymentEntity.getPaymentStatus())
+                .paymentMode(paymentEntity.getPaymentMode())
+                .totalAmount(paymentEntity.getTotalAmount())
+                .paymentDate(paymentEntity.getPaymentDate())
+                .build();
+        log.info("End: PaymentService getPaymentDetailByOrderId");
+        return paymentResponse;
     }
 }
