7783e9446ec02925d525e33691ffe7df1a782051
zizuo <liuzizuo@gmail.com>
2023-10-20 20:13:57 -0700

    create kafka topic amazon

---
+++ KafkaDemo/src/main/java/net/nvsoftware/KafkaDemo/config/KafkaConfig.java
@@ -0,0 +1,14 @@
+package net.nvsoftware.KafkaDemo.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name("amazon").build();
+    }
+}
