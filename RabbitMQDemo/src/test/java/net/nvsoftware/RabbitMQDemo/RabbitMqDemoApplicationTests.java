e14fbdd1e9f5f7a37a7a34d1d4c114b560dcaa41
zizuo <liuzizuo@gmail.com>
2023-10-23 20:34:13 -0700

    setup and config RabbitMQDemo

---
+++ RabbitMQDemo/src/test/java/net/nvsoftware/RabbitMQDemo/RabbitMqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.RabbitMQDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
